bfc2a8685f1301b77e44ca22317218336a891182
selvanponraj <selvan.ponraj@gmail.com>
2020-07-16 23:18:51 +0100

    init files

---
+++ user_data/strategies/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+import os, pkgutil
+# __all__ = list(module for _, module, _ in pkgutil.iter_modules([os.path.dirname(__file__)]))
+__all__=['InformativeSample', 'Strategy001', 'Strategy002', 'Strategy003', 'Strategy004', 'Strategy005', 'berlinguyinca']
---
+++ user_data/strategies/berlinguyinca/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+import os, pkgutil
+# __all__ = list(module for _, module, _ in pkgutil.iter_modules([os.path.dirname(__file__)]))
+__all__ = ['ADXMomentum', 'ASDTSRockwellTrading', 'AdxSmas', 'AverageStrategy', 'AwesomeMacd', 'BbandRsi', 'BinHV27', 'BinHV45', 'CCIStrategy', 'CMCWinner', 'ClucMay72018', 'CofiBitStrategy', 'CombinedBinHAndCluc', 'DoesNothingStrategy', 'EMASkipPump', 'Freqtrade_backtest_validation_freqtrade1', 'Low_BB', 'MACDStrategy', 'MACDStrategy_crossed', 'MultiRSI', 'Quickie', 'ReinforcedAverageStrategy', 'ReinforcedQuickie', 'ReinforcedSmoothScalp', 'Scalp', 'Simple', 'SmoothOperator', 'SmoothScalp', 'TDSequentialStrategy', 'TechnicalExampleStrategy']
